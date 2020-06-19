b1358eb5ac9b4d611a6e9619adb73ced3954813d
KWONTAEDOO <65216835+KWONTAEDOO@users.noreply.github.com>
2020-06-19 14:17:04 +0900

    Add files via upload

---
+++ BirdMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BirdMovement : MonoBehaviour
+{
+
+	[SerializeField]
+
+	private Transform frogTranform;
+
+	private NavMeshAgent birdAgent;
+
+	private Animator _birdAnimator;
+
+	[SerializeField]
+
+	private RandomSound birdFootSteps;
+    // Start is called before the first frame update
+    void Start()
+    {
+		birdAgent = GetComponent<NavMeshAgent>();
+		_birdAnimator = GetComponent<Animator>();
+
+		_birdAnimator.SetBool("Walking", true);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		birdAgent.SetDestination(frogTranform.position);
+
+		if(birdAgent.velocity.magnitude>0)
+		{
+			birdFootSteps.enabled = true;
+		}
+		else
+		{
+			birdFootSteps.enabled = false;
+		}
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			_birdAnimator.SetBool("Attacking", true);
+		}
+	}
+}
