b85ecb2753146a5849200a19c3a274cc29043e8d
KWONTAEDOO <65216835+KWONTAEDOO@users.noreply.github.com>
2020-06-24 23:22:03 +0900

    Update PlayerHealth.cs

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
 	//public이아니라 private여서 캐릭터가 종료가 안됬었다 !!
-	//보고서에 쓰기!!!!!!!!
 	public bool alive;
 
 	[SerializeField]
 	private GameObject pickupPrefab;
-    // Start is called before the first frame update
+   
     void Start()
     { 
 		alive = true;// 게임시작됬을때 살아있다.
